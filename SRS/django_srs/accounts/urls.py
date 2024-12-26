366564bb181cd3d50cf4f326f5fe9ad74a7de513
Rozina-Gebrehiwet <166912787+Rozinagebrehiwet@users.noreply.github.com>
2024-12-26 11:10:19 +0300

    adding comments

--- SRS/django_srs/accounts/urls.py
+++ SRS/django_srs/accounts/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # URL for user login
     path('login/', views.user_login, name='login'),
+    
+    # URL for user logout
     path('logout/', views.user_logout, name='logout'),
 ]
 
