0eae3650efe6c939e41de1de49511e2363fbcc09
Rozina-Gebrehiwet <166912787+Rozinagebrehiwet@users.noreply.github.com>
2024-12-26 11:06:26 +0300

    adding docstrings

--- SRS/django_srs/accounts/forms.py
+++ SRS/django_srs/accounts/forms.py
@@ -1,6 +1,21 @@
 from django import forms
 
-class UserLoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Username"}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"class": "form-control", "placeholder": "Password"}))
 
+class UserLoginForm(forms.Form):
+    """Form for user login."""
+
+    # Username field with styling attributes
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Username"
+        })
+    )
+
+    # Password field with styling attributes
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            "class": "form-control",
+            "placeholder": "Password"
+        })
+    )
