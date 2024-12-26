16d818018e916fa2fde609739a0bc77911b45c4f
Rozina-Gebrehiwet <166912787+Rozinagebrehiwet@users.noreply.github.com>
2024-12-26 11:47:11 +0300

    adding docstrings

--- SRS/django_srs/api/serializers.py
+++ SRS/django_srs/api/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from students.models import StudentShiftInfo
 
+
 class ResultInfoSerializer(serializers.Serializer):
+    """Serializer for student result information."""
+
     board = serializers.CharField(max_length=12)
     roll = serializers.IntegerField()
 
 
 class StudentInfoSerializer(serializers.ModelSerializer):
+    """Serializer for student shift information."""
+
     class Meta:
         model = StudentShiftInfo
         fields = '__all__'
